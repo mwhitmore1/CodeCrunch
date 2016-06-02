0139d413352ea7393457216648e0d585bba1acf6
c-mac88 <christianmcfarland@gmail.com>
2016-06-01 23:40:56 -0700

    Chapter, Module, TrackModule, Question, Answer

---
+++ CodeCrunch.API/CodeCrunch.API/Models/ModuleQuestion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeCrunch.API.Models
+{
+    public class ModuleQuestion
+    {
+        //Primary Key
+        public int ModuleQuestionId { get; set; }
+
+        //Foreign Key
+        public int ModuleId { get; set; }
+
+        //Other fields related to model
+        public string Text { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpVote { get; set; }
+        public int DownVote { get; set; }
+
+
+        //Relationships
+        public Module Module { get; set; }
+        public ModuleAnswer ModuleAnswer { get; set; }
+    }
+}
---
+++ CodeCrunch.API/CodeCrunch.API/Models/TrackModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeCrunch.API.Models
+{
+    public class TrackModule
+    {
+        //Composite Key
+
+
+        //Other fields related to model
+        public Chapter Chapter { get; set; }
+        public Module Module { get; set; }
+    }
+}
