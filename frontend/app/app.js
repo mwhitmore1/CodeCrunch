c848438b8c99417c230aed814c984a5ed8273017
Michael Whitmore <mwhitmorelaw@gmail.com>
2016-06-03 10:13:29 -0700

    add app.js and auth.intercepter.js

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,8 +1,18 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('app', [
-            ''
-        ]);
+(function(){
+	'use strict';
+	angular.module('app', [
+		'ui.router',
+		'LocalStorageModule',
+		'toastr'
+		])
+		 .config(function($stateProvider, $urlRouterProvider, $httpProvider){
+			$urlRouterProvider.otherwise("login");
+			$stateProvider
+			.state('addchapter', { url: '/addchapter', templateUrl: '/addchapter.html', controller: 'AddChapterController as addChapter'})
+			.state('login', { url: '/student/profile', templateUrl: '/student.profile.html', controller: 'StudentProfileController as studentProfile'})
+			.state('posts', { url: '/posts', templateUrl: '/templates/posts.html', controller: 'PostsController as posts'})
+			.state('main', {url: '/main', templateUrl: '/index_main.html', controller: 'MainController as main'});
+			$httpProvider.interceptors.push('authInterceptor');
+		 })	
+		 .value('apiUrl', 'http://localhost:65315/');
 })();
