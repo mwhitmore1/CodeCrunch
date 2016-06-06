97c3bea785e7ca66760dc95012cd5d20ba139440
Andre Edirmanasinghe <andre.edirm@gmail.com>
2016-06-06 16:35:55 -0700

    addChaptersController

---
+++ CodeCrunch.API/CodeCrunch.API/Controllers/ChaptersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CodeCrunch.API.Infrastructure;
+using CodeCrunch.API.Models;
+
+namespace CodeCrunch.API.Controllers
+{
+    public class ChaptersController : ApiController
+    {
+        private UserContext db = new UserContext();
+
+        // GET: api/Chapters
+        public IEnumerable<Chapter> GetChapters()
+        {
+            return db.Chapters;
+        }
+
+        // GET: api/Chapters/5
+        [ResponseType(typeof(Chapter))]
+        public IHttpActionResult GetChapter(int id)
+        {
+            Chapter chapter = db.Chapters.Find(id);
+            if (chapter == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(chapter);
+        }
+
+        // PUT: api/Chapters/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutChapter(int id, Chapter chapter)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != chapter.ChapterId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(chapter).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChapterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Chapters
+        [ResponseType(typeof(Chapter))]
+        public IHttpActionResult PostChapter(Chapter chapter)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Chapters.Add(chapter);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = chapter.ChapterId }, chapter);
+        }
+
+        // DELETE: api/Chapters/5
+        [ResponseType(typeof(Chapter))]
+        public IHttpActionResult DeleteChapter(int id)
+        {
+            Chapter chapter = db.Chapters.Find(id);
+            if (chapter == null)
+            {
+                return NotFound();
+            }
+
+            db.Chapters.Remove(chapter);
+            db.SaveChanges();
+
+            return Ok(chapter);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ChapterExists(int id)
+        {
+            return db.Chapters.Count(e => e.ChapterId == id) > 0;
+        }
+    }
+}
