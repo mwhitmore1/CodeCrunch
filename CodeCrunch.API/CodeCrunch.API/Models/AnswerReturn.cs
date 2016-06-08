ae69071a13dad1d5a6a9352a13f0dff42520e7f9
Michael Whitmore <mwhitmorelaw@gmail.com>
2016-06-08 14:41:49 -0700

    discussion controller started

---
+++ CodeCrunch.API/CodeCrunch.API/Models/AnswerReturn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeCrunch.API.Models
+{
+    public class AnswerReturn
+    {
+        public string UserName { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpVote { get; set; }
+        public int DownVote { get; set; }
+    }
+}
